ecd8a1311af2617a9258e749d610e24d14180dff
orkhanseyfullayev <orkhan.seyfullayev@gmail.com>
2025-11-10 00:06:39 +0300

    added Excel report version.

--- ip_reachability_checker.py
+++ ip_reachability_checker.py
@@ -1,11 +1,24 @@
 import os
+import pandas as pd
 
 with open("ips.txt", "r") as file:
     ip_list = [x.strip() for x in file.readlines()]
 
+results = []
+
+# for ip in ip_list:
+#     response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
+#     if response == 0:
+#         print(f"{ip} is reachable.")
+#     else:
+#         print(f"{ip} is unreachable.")
+
 for ip in ip_list:
     response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
-    if response == 0:
-        print(f"{ip} is reachable.")
-    else:
-        print(f"{ip} is unreachable.")
+    status = "Reachable" if response == 0 else "Unreachable"
+    results.append({"IP Address": ip, "Status": status})
+
+data_result = pd.DataFrame(results)
+# print(data_result)
+
+data_result.to_excel("Report.xlsx", index=False)
