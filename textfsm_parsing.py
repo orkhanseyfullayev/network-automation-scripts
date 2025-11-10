70a5078475d4f7a2810f3f1eb8f39f1b6767f345
orkhanseyfullayev <orkhan.seyfullayev@gmail.com>
2025-11-11 00:24:08 +0300

    adding new  scripts (TextFSM)

---
+++ textfsm_parsing.py
@@ -0,0 +1,22 @@
+import textfsm
+import pandas as pd
+
+raw_output = """
+Interface    IP-Address     OK? Method Status Protocol
+Gig0/0       192.168.1.1    YES manual up     up
+Gig0/1       unassigned     YES unset  down   down
+"""
+
+# Template dosyasını oku
+with open("textfsm_template.textfsm") as template:
+    fsm = textfsm.TextFSM(template)
+
+parsed = fsm.ParseText(raw_output)
+
+
+# print(fsm.header)
+# print(parsed)
+
+
+df = pd.DataFrame(parsed, columns=fsm.header)
+print(df)
